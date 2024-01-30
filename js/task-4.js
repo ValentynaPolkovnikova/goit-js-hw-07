55a78b0a5c6a5ba0bc90f16bad1f1b1419c91f4e
ValentynaPolkovnikova <137114414+ValentynaPolkovnikova@users.noreply.github.com>
2024-01-30 16:46:36 +0100

    fix task 4

--- js/task-4.js
+++ js/task-4.js
@@ -2,18 +2,17 @@ const form = document.querySelector('.login-form');
 
 function handleSubmit(event){
     event.preventDefault();
-   const emailInput = foregisterForm.elements.email;
-   const passwordInput = registerForm.elements.password;
+   const emailInput = form.elements.email;  // Fix variable name to 'form'
+   const passwordInput = form.elements.password; // Fix variable name to 'form'
    const emailValue = emailInput.value.trim();
    const passwordValue = passwordInput.value.trim();
-    
-if(emailValue === "" || passwordValue ===""){
+ 
+if (emailValue === "" || passwordValue ===""){
     return alert('All form fields must be filled in')
     }
-else{  
-   console.log(`email: ${emailValue},
-                password: ${passwordValue}`);
-   registerForm.reset(); // Move the reset inside the 'else' block
-    }
+else {  
+   console.log({ email: emailValue, password: passwordValue }); // Change console.log format
+   form.reset();                                                // Move the reset inside the 'else' block
+   }
 }
-registerForm.addEventListener('submit', handleSubmit);
+form.addEventListener('submit', handleSubmit);
